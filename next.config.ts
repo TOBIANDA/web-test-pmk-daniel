290f8ec70c006a3408d59979dd24fa0b80de2344
TOBIANDA <galeribelajartobi@gmail.com>
2026-08-27 11:09:46 +0700

    fix(images): enable unoptimized true globally in next.config.ts for full crisp image quality

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
@@ -14,8 +15,6 @@ const nextConfig: NextConfig = {
         hostname: "**",
       },
     ],
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
   async rewrites() {
     const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:8000";
